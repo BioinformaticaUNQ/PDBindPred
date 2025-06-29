577e7eff451b749266620fb435c82c18a4933e3c
cintuza <cinbiblio@yahoo.com.ar>
2025-06-28 21:37:27 -0300

    tests get ids

--- PDBindPred/tests/main_tests.py
+++ PDBindPred/tests/main_tests.py
@@ -1,8 +1,21 @@
+from PDBindPred import main
 import unittest
 
 class Tests(unittest.TestCase):
-    def test_1(self):
-        self.assertEqual((1+1), 2)
+    def test_get_uniprot_id_from_existing_pdb_id(self):
+        pdb_id = "6IK4"
+        uniprot_id = main.get_uniprot_id_from_pdb_id(pdb_id)
+        self.assertEqual("E7D102", uniprot_id)
+
+    def test_get_chembl_id_from_uniprot_id(self):
+        uniprot_id = "P61812"
+        chembl_id = main.get_chembl_id_from_uniprot_id(uniprot_id)
+        self.assertEqual("CHEMBL3217393", chembl_id)
+
+    def test_get_chembl_id_from_pdb_id(self):
+        pdb_id = "1CY0"
+        chembl_id = main.get_chembl_id_from_pdb_id(pdb_id)
+        self.assertEqual("CHEMBL3259513", chembl_id)
 
 if __name__ == '__main__':
     unittest.main()
