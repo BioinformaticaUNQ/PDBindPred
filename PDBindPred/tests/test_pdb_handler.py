6fb85489b886b7388609ba80ceb9c45796752691
cintuza <cinbiblio@yahoo.com.ar>
2025-07-08 19:25:57 -0300

    init test_pdb_handler

---
+++ PDBindPred/tests/test_pdb_handler.py
@@ -0,0 +1,17 @@
+import unittest
+
+from PDBindPred.pdb_handler import fetch_pdb_info
+
+
+class TestsPdbHandler(unittest.TestCase):
+    def test_fetch_pdb_info_successfully(self):
+        pdb_id = '1MQ8'
+        pdb_id_data = fetch_pdb_info(pdb_id)
+        self.assertEqual('1MQ8', pdb_id_data['pdb_id'])
+        self.assertEqual(3.3, pdb_id_data['resolution'])
+        self.assertEqual('2003', pdb_id_data['publication_year'])
+        self.assertEqual('10.1016/S0092-8674(02)01257-6', pdb_id_data['doi'])
+        self.assertEqual([], pdb_id_data['ligands'])
+
+if __name__ == '__main__':
+    unittest.main()
